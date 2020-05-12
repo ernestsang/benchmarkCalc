38ce415ca336258cd79a0467d16d0b09e753f835
Ernest <tsangernest@hotmail.com>
2020-05-12 15:14:16 -0500

    found a non-efficient way to grab rows and columns and last msg of the night

--- benchCalc.py
+++ benchCalc.py
@@ -1,1 +1,19 @@
-hello
+import sys
+import os
+import pandas as pd
+
+
+#data = pd.read_csv('TEST_INPUT_EFT.hml', encoding='latin1',
+# skiprows=range(0, 18), usecols=range(2, 14))
+
+#data = pd.read_csv('TEST_INPUT_EFT.hml', skiprows=range(0,18), usecols=['CPU Package'])
+
+df = pd.read_csv('TEST_INPUT_EFT.hml', skiprows=range(0,2),
+		 usecols=range(2,14), encoding='iso-8859-1')
+
+print(df)
+
+
+
+print("**End of Processing***")
+
