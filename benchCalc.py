2125eb773e90a3da62eb81ebe5b5538e7e90c47f
Ernest <tsangernest@hotmail.com>
2020-05-12 15:36:52 -0500

    figured out how to use python arrays

--- benchCalc.py
+++ benchCalc.py
@@ -8,11 +8,20 @@ import pandas as pd
 
 #data = pd.read_csv('TEST_INPUT_EFT.hml', skiprows=range(0,18), usecols=['CPU Package'])
 
+columnNames = []
+
+#getInputFile
 df = pd.read_csv('TEST_INPUT_EFT.hml', skiprows=range(0,2),
-		 usecols=range(2,14), encoding='iso-8859-1')
+		 usecols=range(2,14),  encoding='cp1252')
 
 print(df)
 
+#get column names
+for words in df:
+	columnNames.append(words)
+
+print(columnNames)
+#newDF = pd.DataFrame(columns =)
 
 
 print("**End of Processing***")
