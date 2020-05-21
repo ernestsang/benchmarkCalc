6aa6e9291555aea9f78664e4193c4dc93f2dc513
Ernest <tsangernest@hotmail.com>
2020-05-21 18:07:30 -0500

    can calculate 1% and 0.1%.

--- benchCalc_by_name.py
+++ benchCalc_by_name.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import math
 
+FPS_MIN = 2
+FPS_MAX = 402 #since CSGO max is 400
 
 #get inputfile via command line
 #auto skips first 2 rows and uses the 3rd row which is 
@@ -13,7 +15,7 @@ df = pd.read_csv(inputFile, skiprows=range(0,2), usecols=range(2,14),
 
 #============================================
 #delete unit rows, which is the next 12
-df = df.iloc[15:,] #12 rows for witcher, #15 for eft
+df = df.iloc[12:,] #12 rows for witcher, #15 for eft
 #needs work to make sure it's dynamic, to skip rows with units
 #=========================================
 
@@ -24,16 +26,35 @@ df = df.apply(pd.to_numeric)
 #then assign back into column
 df = df.rename(str.strip, axis='columns')
 
-#quick description of dataframe
-print(df.describe())
+#quick description of dataframe before sorting and deleting, raw dataframe
+print(df.describe(), "\n====================================================\n")
 
 fpsValues = df['Framerate'].sort_values()
-fpsValues = fpsValues[fpsValues > 2] #drop all fps values below 2
-fpsValues = fpsValues.reset_index(drop=True)#shouldn't need to reset_index twice..
+
+#store all fpsValues that are above min and below max
+fpsValues = fpsValues[(fpsValues > FPS_MIN) & (fpsValues < FPS_MAX)] 
+
+#reset index values that do not meet requirement of FPS_MIN and FPS_MAX
+fpsValues = fpsValues.reset_index(drop=True)
 
 print(fpsValues.describe())
 
-print(fpsValues)
+#get num of percent lows for 1% and 0.1%
+onePercentValues = math.ceil(len(fpsValues) * 0.01) #1% of total values
+pointOneValues = math.ceil(len(fpsValues) * 0.001) #0.1% of total values
+
+avgOnePercentFPS = fpsValues.iloc[0:onePercentValues].mean()
+avgPointOnePercentFPS = fpsValues.iloc[0:pointOneValues].mean()
+
+print("\n\nFPS 1% lows are = ", avgOnePercentFPS.round())
+print("Values used for 1% = ", onePercentValues)
+
+print("\nFPS 0.1% lows are = ", avgPointOnePercentFPS.round())
+print("Values used for 0.1% = ", pointOneValues)
+
+if onePercentValues < 200:
+	print("...You should probably collect more data points...")
+
 
 print("\n\n***End of Processing***")
 
